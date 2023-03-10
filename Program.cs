f0db8828dddebf09b3da9bf1b3f6a321092e8388
Meghrig <76222303+meghrigJAC@users.noreply.github.com>
2023-03-09 20:09:33 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿namespace Exceptions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] examScore;
+            int totalScores = 0;
+            int countOfScores = 0;
+            string inValue;
+            double averageTestScore;
+
+
+            try
+            {
+                Console.Write("How many scores will you enter? ");
+                inValue = Console.ReadLine();
+                countOfScores = int.Parse(inValue);
+                examScore = new int[countOfScores];
+
+                for (int i = 0; i < countOfScores; i++)
+                {
+                    Console.Write("Enter score {0}: ", i + 1);
+                    inValue = Console.ReadLine();
+                    examScore[i] = int.Parse(inValue);
+                    totalScores += i;
+                }
+
+                averageTestScore = totalScores / countOfScores;
+                Console.WriteLine("Average is {0}", averageTestScore);
+            }
+            catch (Exception e)
+            {
+                //TODO
+            }
+            //add a finally
+
+            ReadKey();
+
+        }
+    }
+}
